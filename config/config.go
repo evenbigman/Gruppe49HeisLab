57ed0f3ae240698799a4162ba351c7a9447982b8
Ole Sværen <ole.svaeren@gmail.com>
2026-03-16 13:56:19 +0100

    Created first version of main

--- config/config.go
+++ config/config.go
@@ -9,5 +9,7 @@ const (
 	BcastInterval     = 4000 //ms
 	TimeoutInterval   = 3000
 	DoorOpenTime      = 3 * time.Second
+	InitDelay         = 5 * time.Second
+	TimeoutAck        = 500 * time.Second
 	DefaultElevioPort = "15657"
 )
--- main.go
+++ main.go
@@ -2,28 +2,61 @@ package main
 
 import (
 	backup "sanntidslab/backup_handler"
+	"sanntidslab/config"
 	"sanntidslab/controller"
+	hallrequestassigner "sanntidslab/hall_request_assigner"
 	"sanntidslab/peers"
+	"time"
 )
 
 func main() {
 	backup.Init()
 
-	ElevatorController := controller.GetController()
-	ElevatorController.InitElevator()
-	ElevatorController.Start()
-	elevator := ElevatorController.GetElevatorState()
-
 	peerManager := peers.NewPeerManager()
 	peerManager.Init()
 	peerManager.Run()
 
+	pm := peers.NewPeerManager()
+
+	pm.Init()
+	go pm.Run()
+
+	time.Sleep(config.InitDelay)
+	ec := controller.GetController()
+	startSnapshot, err := pm.GetMySnapshot()
+
+	if err == nil {
+		ec.InitElevatorWithStates(startSnapshot.Elevator)
+	} else {
+		ec.InitElevator()
+	}
+
+	buttonCh := ec.SubscribeButtons()
+	stateCh := ec.SubscribeState()
+
+	ec.Start()
+
 	for {
 		select {
-		case order := <-peerManager.NewOrderCh:
-			// Something
-		case disconnectedPeers := <-peerManager.DisconnectedPeerCh:
-			// Something
+		case <-peerManager.NewOrderCh:
+			// assign orders from others
+		case <-peerManager.DisconnectedPeerCh:
+			// Redistribute orders
+		case <-buttonCh:
+			stateToAck := ec.GetElevatorState()
+			go func() {
+				err := pm.WaitForAck(stateToAck, config.TimeoutAck)
+				if err != nil {
+				} else {
+					ec.SetGlobalHallOrders(stateToAck.PressedHallButtons)
+					// TODO: Add hallrequest assigner
+					ec.SetCabOrders(stateToAck.PressedCabButtons)
+				}
+			}()
+		case <-stateCh:
+			state := ec.GetElevatorState()
+			pm.SetMySnapshot(state)
 		}
 	}
+
 }
