70d3c1cc8b05e8af8eb88d2aaebab4ae253f168c
Even Fjeldstad <evenmf@gmail.com>
2026-03-17 13:37:08 +0100

    less slow config

--- config/config.go
+++ config/config.go
@@ -6,10 +6,10 @@ const (
 	NumFloors         = 4
 	MaxFloor          = NumFloors - 1
 	BcastPort         = 16569
-	BcastInterval     = 4000 //ms
-	TimeoutInterval   = 3000
+	BcastInterval     = 250 //ms
+	TimeoutInterval   = 2000
 	DoorOpenTime      = 3 * time.Second
 	InitDelay         = 5 * time.Second
-	TimeoutAck        = 500 * time.Second
+	TimeoutAck        = 1000 * time.Second
 	DefaultElevioPort = "15657"
 )
